2ed7e46063317e394088a95996694481b3de2d21
hkgnp <ben@pngs.cc>
2022-02-13 03:17:50 +0800

    minor refactor

--- src/convert.ts
+++ src/convert.ts
@@ -1,8 +1,6 @@
 export const convert = (data: string) => {
   const matchData = data.match(/```mermaid(.|\n)*?```/gm);
 
-  console.log(matchData[0]);
-
   let toDecode = matchData[0];
   toDecode = toDecode.replace('```mermaid', '').replace('```', '');
   toDecode = toDecode.replace('\n', ' ');
