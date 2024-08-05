a9f70c2774ce45165b54bafb5b04ce9295a9958e
Benjy <ben@pngs.cc>
2024-08-06 00:59:36 +0800

    BREAKING CHANGE: Move plugin to completely local
    Move away from mermaid.ink to a local mermaid + blob

--- src/css.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '*.css?raw' {
-  const content: string
-  export default content
-}
--- src/services/check-if-theme.ts
+++
@@ -1,9 +0,0 @@
-type Theme = 'default' | 'neutral' | 'dark' | 'forest'
-
-export const checkValidTheme = (theme: unknown): Theme => {
-  const validThemes: Theme[] = ['default', 'neutral', 'dark', 'forest']
-  if (typeof theme === 'string' && validThemes.includes(theme as Theme)) {
-    return theme as Theme
-  }
-  return 'default'
-}
